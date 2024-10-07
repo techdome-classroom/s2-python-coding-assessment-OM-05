31ca80446f271a5138a7c8b9c8ab28e217049f31
Om bagresh <ombagresh12345@gmail.com>
2024-10-07 14:10:58 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -8,6 +8,24 @@ class Solution(object):
 
 
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
+
+# Ensure there's an instance of the class to run if needed
+if __name__ == "__main__":
+    solution = Solution()
 
 
 
